4b429e5b7e64003826865d9a330fccaed1530953
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-05-04 00:34:48 +0900

    add route

---
+++ src/components/AutoKana.vue
@@ -0,0 +1,36 @@
+<template>
+  <input @keyup="onKeyUp" />
+</template>
+
+<script>
+import historykana from 'historykana'
+
+var logic = (history, value) => {
+  history.push(value)
+  return historykana(history)
+}
+
+export default {
+  name,
+  data () {
+    return {
+      name,
+      kana: '',
+      _value: '',
+      history: []
+    }
+  },
+  props: {
+    value: String,
+    onChange: Function
+  },
+  methods: {
+    onKeyUp: function ({target: {value: _value = ''}}) {
+      this.kana = logic(this.history, _value)
+      this.onChange(this.kana)
+    }
+  }
+}
+</script>
+<style>
+</style>
---
+++ src/pages/AutoKanaTest.vue
@@ -0,0 +1,40 @@
+<template>
+<v-container fluid :class="[name, 'scroll-y']">
+  <AutoKana :onChange="test" />
+  {{testValue}}
+</v-container>
+</template>
+
+<script>
+import AutoKana from '@/components/AutoKana'
+const name = __filename.match(/^src\/[\w|\\/]*\/(\w*).vue$/)[1]
+
+export default {
+  name,
+  computed: {
+  },
+  data () {
+    return {
+      name,
+      testValue: ''
+    }
+  },
+  methods: {
+    test: function (value) {
+      console.log(value)
+      this.testValue = value
+    }
+  },
+  components: {
+    AutoKana
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+.Blog
+  height 100vh
+  overflow auto
+
+
+</style>
