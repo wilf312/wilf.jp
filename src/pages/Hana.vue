7034d53470cd72097ab4fe8de5dc111fe7309c1d
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-05-09 01:11:18 +0900

    hiragana

--- src/pages/Hana.vue
+++ src/pages/Hana.vue
@@ -1,35 +1,48 @@
 <template>
   <div :class="name">
+    <v-radio-group v-model="type" :mandatory="false">
+      <v-radio label="はなびら" value="hanabira"></v-radio>
+      <v-radio label="ひらがな" value="hiragana"></v-radio>
+    </v-radio-group>
     <div class="anime-wrap">
-      <Hanabira
-        :path="list[(n % 7)]"
-        :t="t"
-        :w="dimension.w"
-        :h="dimension.h"
-        v-for="n in cnt" :key="n" />
+      <div v-if="type === 'hanabira'">
+        <Hanabira
+          :path="list[(n % 7)]"
+          :t="t"
+          :w="dimension.w"
+          :h="dimension.h"
+          v-for="n in cnt" :key="n" />
+      </div>
+      <div v-else>
+        <Hiragana
+          :t="t"
+          :w="dimension.w"
+          :h="dimension.h"
+          v-for="n in cnt" :key="n" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Hanabira from '-/Hanabira'
-import {mapGetters, mapState} from 'vuex'
-
-import Img1 from 'assets/1.png'
-import Img2 from 'assets/2.png'
-import Img3 from 'assets/3.png'
-import Img4 from 'assets/4.png'
-import Img5 from 'assets/5.png'
-import Img6 from 'assets/6.png'
-import Img7 from 'assets/7.png'
+import {mapGetters} from 'vuex'
+import Hanabira from '@/components/Hanabira'
+import Hiragana from '@/components/Hiragana'
+import Img1 from '../assets/1.png'
+import Img2 from '../assets/2.png'
+import Img3 from '../assets/3.png'
+import Img4 from '../assets/4.png'
+import Img5 from '../assets/5.png'
+import Img6 from '../assets/6.png'
+import Img7 from '../assets/7.png'
 const name = __filename.match(/^src\/[\w|\\/]*\/(\w*).vue$/)[1]
-console.log(name)
 
 export default {
   name,
   data () {
     return {
       name,
+      type: 'hiragana',
       t: 0,
       cnt: 1,
       plus: 1,
@@ -45,7 +58,7 @@ export default {
     }
   },
   computed: {
-    ...mapState('ua', ['ua']),
+    ...mapGetters('ua', ['ua']),
     ...mapGetters('common', ['dimension'])
   },
   mounted () {
@@ -72,16 +85,27 @@ export default {
     timerFunc()
   },
   components: {
-    Hanabira
+    Hanabira,
+    Hiragana
+  },
+  methods: {
+    changeType (_type) {
+      this.type = _type
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-.Hana
-  width 100vw
-  height 100vh
-  position fixed
-  overflow hidden
+  .Hana
+    width 100vw
+    height 100vh
+    position fixed
+    overflow hidden
+
+    h1
+      position absolute
+      top 50vh
+      font-size 20rem
 </style>
