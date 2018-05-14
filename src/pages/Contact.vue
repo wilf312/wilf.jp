522476a2722b2742cf45a6e150c1447c5b8ce810
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-05-15 03:03:51 +0900

    fix

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,24 +1,31 @@
 <template>
   <div>
-    <form name="contact" action="" method="post" netlify>
-      <p>
-        <v-text-field
-                name="name"
-                label="name"
-        ></v-text-field>
-      </p>
-      <p>
-        <v-text-field
-                name="email"
-                label="email"
-        ></v-text-field>
-      </p>
-      <div data-netlify-recaptcha></div>
-      <p>
-        <input class="form-button" type="submit" value="Send message" />
-      </p>
-      <input type="hidden" name="form-name" value="contact" />
-    </form>
+    <div class="container">
+      <h1 class="title">
+        Contact
+      </h1>
+      <div class="content">
+        <form name="contact" action="/thank-you" netlify-honeypot="bot-field" method="post" netlify>
+          <input type="hidden" name="form-name" value="contact" />
+          <p class="hidden">
+            <label>Don’t fill this out: <input name="bot-field"></label>
+          </p>
+          <label class="form-label" for="name">
+            Name:
+          </label>
+          <input class="form-field" name="name" id="name" />
+          <label class="form-label" for="email">
+            Email:
+          </label>
+          <input class="form-field" name="email" id="email" />
+          <label class="form-label" for="message">
+            Message:
+          </label>
+          <textarea class="form-field" name="message" id="message"></textarea>
+          <input class="form-button" type="submit" value="Send message" />
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
